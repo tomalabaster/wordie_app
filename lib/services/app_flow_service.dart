c6f4e60bcac35f12743a3e11374df5a12e277b54
Tom Alabaster <tomalabasteruk@gmail.com>
2019-02-20 18:14:41 +0000

    Temporary caching for speed.

--- lib/services/app_flow_service.dart
+++ lib/services/app_flow_service.dart
@@ -45,27 +45,35 @@ class FirebaseAppFlowService extends IAppFlowService {
 
   FirebaseAppFlowService(this._userStore);
 
+  bool _hasHadTodaysSkip;
+
   @override
   Future<bool> hasHadTodaysSkip() async {
-    var snapshot = await this._userStore.get();
+    if (this._hasHadTodaysSkip == null) {
+      var snapshot = await this._userStore.get();
 
-    if (snapshot.data.containsKey("dateLastSkipped")) {
-      var dateTimeLastSkipped = DateTime.parse(snapshot.data["dateLastSkipped"]);
-      var dateLastSkipped = DateTime(dateTimeLastSkipped.year, dateTimeLastSkipped.month, dateTimeLastSkipped.day);
-      var today = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+      if (snapshot.data.containsKey("dateLastSkipped")) {
+        var dateTimeLastSkipped = DateTime.parse(snapshot.data["dateLastSkipped"]);
+        var dateLastSkipped = DateTime(dateTimeLastSkipped.year, dateTimeLastSkipped.month, dateTimeLastSkipped.day);
+        var today = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
 
-      return !dateLastSkipped.isBefore(today);
-    } else {
-      return false;
+        this._hasHadTodaysSkip = !dateLastSkipped.isBefore(today);
+      } else {
+        this._hasHadTodaysSkip = false;
+      }
     }
+
+    return this._hasHadTodaysSkip;
   }
 
   @override
   Future<void> setHasHadTodaysSkip(bool hasHadTodaysSkip) async {
-    Map<String, dynamic> data = {
-      "dateLastSkipped": DateTime.now().toString()
-    };
+    this._hasHadTodaysSkip = hasHadTodaysSkip;
+
+    var data = (await this._userStore.get()).data;
+
+    data["dateLastSkipped"] = DateTime.now().toIso8601String();
     
-    await Firestore.instance.collection('users').document((await FirebaseAuth.instance.currentUser()).uid).updateData(data);
+    await this._userStore.updateData(data);
   }
 }
